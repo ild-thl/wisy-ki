3ef00f71a42a157f24078dc52c1b9efec29b3b9b
Pascal Hürten <pascal.huerten@th-luebeck.de>
2024-01-11 19:31:35 +0100

    Remove ki_admin to readd later as submodule

--- ki_admin/config/index_plugin_stichwoerter_0.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once('index_plugin_multiedit.inc.php');
-$multiedit = new MULTIEDIT_PLUGIN_CLASS;
-$multiedit->main('stichwoerter');
--- ki_admin/log.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once('functions.inc.php');
-$ob = new LOG_RENDERER_CLASS;
-$ob->handle_request();
